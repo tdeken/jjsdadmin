8b96d9f882848b50535b8fd1fe5d282508ef2e64
eken <eken@branchcn.com>
2025-12-16 17:18:27 +0800

    bug fix

--- apps/web-antd/src/views/order/print.vue
+++ apps/web-antd/src/views/order/print.vue
@@ -3,7 +3,7 @@
     <div ref="topElRef">
       <div>
         <div class="p-label-g p-title">
-          <div class="p-label" style="width: 100%;font-weight: bold; font-size: 20px;text-align: center;">
+          <div class="p-label" style="width: 100%;font-weight: bold; font-size: 12pt;text-align: center;">
             健健综合店 (批 发/进 货 台 账)
           </div>
         </div>
@@ -28,13 +28,13 @@
     <Grid />
 
     <div ref="bottomElRef">
-      <div style="margin-top: 5px;">
-        <div class="p-label-g" style="font-size: 14px;">
+      <div style="margin-top: 2px;">
+        <div class="p-label-f">
           <div class="p-label p-label-30">开票人：{{ data?.operator }}</div>
           <div class="p-label p-label-30">配送员：{{ data?.deliver }}</div>
           <div class="p-label p-label-40">共 {{ data?.total }} 样商品</div>
         </div>
-        <div class="p-label-g" style="font-size: 14px;">
+        <div class="p-label-f">
           特别提示：尊敬的客户，请与配送员当面点清货物，如有错误，请及时来电，多谢惠顾
         </div>
       </div>
@@ -93,7 +93,7 @@ const gridOptions: VxeGridProps<PrintSku> = {
   border: "full",
   showOverflow: true,
   columns: [
-    { field: 'number', title: '商品条码', width: 110 },
+    { field: 'number', title: '商品条码', width: 120 },
     { field: 'name', title: '商品名称', minWidth: 150 },
     { field: 'book_num', title: '数量', width:50, align: 'center' },
     { field: 'unit', title: '单位', width:50, align: 'center' },
@@ -119,8 +119,8 @@ const bottomElRef = ref<HTMLDivElement>()
 
 const customStyle = `
   @page { 
-    size: auto;
-    margin: 0 !important; 
+    size: 225mm 110mm;
+    margin: 0 !important;
   }
     
   body {
@@ -134,7 +134,7 @@ const customStyle = `
   .vxe-table th,
   .vxe-table td {
     border: 1px solid #000 !important;
-    font-size: ` + '14' + `px;
+    font-size: ` + '8' + `pt;
   }
   .p-label {
     float: left;
@@ -151,14 +151,22 @@ const customStyle = `
   .p-label-g {
     display: inline-block; 
     width: 100%;
-    height: 18px;
-    line-height: 20px;
-    font-size: ` + '16' + `px;
+    height: 16px;
+    line-height: 16px;
+    font-size: ` + '9' + `pt;
+  }
+
+  .p-label-f {
+    display: inline-block; 
+    width: 100%;
+    height: 12px;
+    line-height: 12px;
+    font-size: ` + '8' + `pt;
   }
   
   .p-title {
-    margin-bottom: 8px;
-    margin-top: 14px;
+    margin-bottom: 5px;
+    margin-top: 10px;
   }
 
 `
